a7d5b7552e323c70a3fe6e1f5199c504e2ead172
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-03 20:27:08 +0200

    fetch recipe categories

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  categories = {};
 
   constructor() { }
 
   ngOnInit() {
+    this.fetchCategories();
   }
+
+  fetchCategories() {
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.categories = responseJson.meals;
+      });
+  }
+
 
 }
