ee6df67b661dbc27b825fa5b7264ba58802b5737
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-03 20:06:43 +0200

    generate nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
