a033eed38047b54fa77a987d99f9fe4f7b85d100
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-03 20:22:34 +0200

    generate categories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavComponent } from './nav/nav.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    NavComponent
+    NavComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
