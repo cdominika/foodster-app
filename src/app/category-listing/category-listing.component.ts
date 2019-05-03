805d7bf08c66264f487804a459b027c50e4b5dc5
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-03 22:56:25 +0200

    fetch dishes function for beef category

--- src/app/category-listing/category-listing.component.ts
+++ src/app/category-listing/category-listing.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-listing.component.css']
 })
 export class CategoryListingComponent implements OnInit {
-
+  dishes = {};
   constructor() { }
 
   ngOnInit() {
+    this.fetchDishes();
+  }
+
+  fetchDishes() {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=beef`)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.dishes = responseJson;
+        console.log(this.dishes);
+      });
   }
 
 }
