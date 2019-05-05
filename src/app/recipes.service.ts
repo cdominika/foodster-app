bcc0fc5d3c3d321a9744e311d92ceee89644054f
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-05 23:06:39 +0200

    add http get request to the endpoint

--- src/app/recipes.service.ts
+++ src/app/recipes.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
-  apiURL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
-  constructor(private httpClient: HttpClient) { }
+  apiURL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772'; // przykład
+  constructor(private http: HttpClient) {}
+    fetchRecipes(): Observable<object> {
+      return this.http.get(this.apiURL);
+    }
 }
