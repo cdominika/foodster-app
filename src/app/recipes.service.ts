f379adf9bef75c06cf53868ba29a00bd0344e8a8
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-05 22:53:12 +0200

    inject HttpClient and add apiURL string variable

--- src/app/recipes.service.ts
+++ src/app/recipes.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
-
-  constructor() { }
+  apiURL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+  constructor(private httpClient: HttpClient) { }
 }
