6bb154cc8fd0440ac960a188feb0320926f6520a
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-26 01:16:40 +0200

    remove displaying of null and undefined ingredients, add to cache

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -17,17 +17,19 @@ export class MainComponent implements OnInit {
               private route: ActivatedRoute,
               private app: AppComponent) {
     const randomFromCache = JSON.parse(localStorage.getItem('random'));
+    const ingredientsFromCache = JSON.parse(localStorage.getItem('ingredients'));
     if (randomFromCache !== null || randomFromCache !== {}) {
       this.random = randomFromCache;
+      this.ingredients = ingredientsFromCache;
       console.warn(randomFromCache);
     }
   }
 
   ngOnInit() {
-    if (this.random === null) {
+    if (this.random === null ) {
       this.fetchRandomRecipe();
     }
-    this.app.setTitle(`Tasty Recipes and Food Inspirations | Foodster`);
+    this.app.setTitle(`Tasty Recipes and Food Inspirations | Foodser`);
   }
 
     fetchRandomRecipe() {
@@ -37,14 +39,19 @@ export class MainComponent implements OnInit {
           this.random = data.meals[0];
           localStorage.setItem('random', JSON.stringify(data.meals[0]));
           this.listIngredients();
+          localStorage.setItem('ingredients', JSON.stringify(this.ingredients));
         });
     }
 
     listIngredients() {
-    for (let i = 0; i < 21; i++) {
+    this.ingredients = [];
+    for (let i = 1; i < 21; i++) {
       console.log(this.random[`strIngredient${i}`]);
       let ingredient;
-      if (this.random[`strMeasure${i}`] !== undefined || this.random[`strIngredient${i}`] !== undefined) {
+      if (this.random[`strMeasure${i}`] !== null
+        && this.random[`strMeasure${i}`] !== undefined
+        && this.random[`strIngredient${i}`] !== undefined
+        && this.random[`strIngredient${i}`] !== null) {
         ingredient = `${this.random[`strMeasure${i}`]} ${this.random[`strIngredient${i}`]}`;
         this.ingredients = [...this.ingredients, ingredient];
       }
