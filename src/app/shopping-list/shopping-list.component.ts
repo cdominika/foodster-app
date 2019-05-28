b184d041a5db46862812e806d8ae605019134282
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-29 00:09:21 +0200

    get shopping list from localstorage

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ShoppingListComponent implements OnInit {
 
   constructor() { }
+  list;
 
   ngOnInit() {
+    this.list = localStorage.getItem('shopping list');
+    console.log(this.list);
   }
 
 }
