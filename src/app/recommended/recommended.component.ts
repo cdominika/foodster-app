70148849b0a2eb2c1b82248b8fdc156def3138f4
cdominika <dominika.cygankiewicz@gmail.com>
2019-05-30 14:44:08 +0200

    add fetchRecommended function

--- src/app/recommended/recommended.component.ts
+++ src/app/recommended/recommended.component.ts
@@ -9,12 +9,26 @@ import { RecipesService } from '../recipes.service';
 export class RecommendedComponent implements OnInit {
 
   constructor(private recipesService: RecipesService) { }
-  dishes = [];
+  dishes;
+  categoryNames = [];
 
   ngOnInit() {
     let categories = JSON.parse(localStorage.getItem('nav'));
     console.log(categories);
-
+    for (let category of categories.categories) {
+      this.categoryNames = [...this.categoryNames, category.strCategory]
+    }
+    console.log(this.categoryNames);
+    this.fetchRecommended();
   }
 
+fetchRecommended() {
+  for (let i = 0; i < 3; i++)
+  this.recipesService.fetchCategories(this.categoryNames[i].toLowerCase())
+  .subscribe((dishes) => {
+    console.warn(dishes);
+    this.dishes = dishes;
+    localStorage.setItem(`recommended ${this.categoryNames[i]}`, JSON.stringify(this.dishes));
+  });
+}
 }
